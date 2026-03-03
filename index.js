41fef961af3274f29c553f7bfd603acfe88f32e7
demosvox <demos.voxemails@gmail.com>
2026-03-04 00:44:40 +0545

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const EMPLOYEE_MAP = {
   "st_ronan": "Ronan",
   "yooloo12": "Bipul",
   "upsahnapoudel.": "Upsahna",
+  "samm3110": "Sambid",
+  
 };
 
 // ─── GOOGLE SHEETS AUTH ────────────────────────────────────────
